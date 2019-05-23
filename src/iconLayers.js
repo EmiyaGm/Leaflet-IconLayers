97365ae8d5e936fefd59284e6d3740cd16e8a747
龚旻 <min.gong@d3j.io>
2019-05-23 13:53:09 +0800

    fix conflicts

--- src/iconLayers.js
+++ src/iconLayers.js
@@ -70,7 +70,7 @@
     }
 
     var IconLayers = L.Control.extend({
-        includes: L.Mixin.Events,
+        includes: L.Evented ? L.Evented.prototype : L.Mixin.Events,
         _getActiveLayer: function() {
             if (this._activeLayerId) {
                 return this._layers[this._activeLayerId];
